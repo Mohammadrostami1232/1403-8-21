3e6708d45a9a79cded9b2af9c6ac13734ecb9765
Mohammadrostami1232 <148641750+Mohammadrostami1232@users.noreply.github.com>
2024-11-10 03:58:21 +0330

    Add files via upload

---
+++ Form11.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public class Vehicle
+        {
+            public virtual void Drive()
+            {
+
+            }
+        }
+        public class Car : Vehicle
+        {
+            public override void Drive()
+            {
+
+            } 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Car mycar = new Car();
+            mycar.Drive();
+        }
+    }
+}
